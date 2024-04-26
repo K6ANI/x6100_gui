2fb09cdfa755b8d7970024a4f36cfcde47a4c17b
K6ANI <57457671+K6ANI@users.noreply.github.com>
2024-04-26 15:13:11 -0700

    Corrected qth.c for the latest Grid Specifications
    Corrected the Grid check processing in the qth.c file. Added over- and under-range checks to the position to Grid processing. Added checks for invalid Grids where needed.

--- src/qth.c
+++ src/qth.c
@@ -30,26 +30,38 @@ void qth_update(const char *qth) {
 }
 
 bool grid_check(const char *grid) {
+
+    /* Jerry Shaw K6ANI
+    Corrected the code to check for all 4 active fields. Changed the range
+    of each alphabetic field to correspond with the current Grid Square
+    specification ranges.
+    */
+
     uint8_t len = strlen(grid);
-    
-    switch (len) {
-        case 8:
-            if (grid[7] < '0' || grid[7] > '9') return false;
-            if (grid[6] < '0' || grid[6] > '9') return false;
-        case 6:
-            if (toupper(grid[5]) < 'A' || toupper(grid[5]) > 'S') return false;
-            if (toupper(grid[4]) < 'A' || toupper(grid[4]) > 'S') return false;
-        case 4:
-            if (grid[3] < '0' || grid[3] > '9') return false;
-            if (grid[2] < '0' || grid[2] > '9') return false;
-        case 2:
-            if (toupper(grid[1]) < 'A' || toupper(grid[1]) > 'S') return false;
-            if (toupper(grid[0]) < 'A' || toupper(grid[0]) > 'S') return false;
-            break;
-            
-        default:
-            return false;
+
+    if (!((len == 8) || (len == 6) || (len == 4) || (len == 2)))
+        return false;
+
+    if (len == 8)
+    {
+        if (grid[7] < '0' || grid[7] > '9') return false;
+        if (grid[6] < '0' || grid[6] > '9') return false;
     }
+
+    if (len >= 6)
+    {
+        if (toupper(grid[5]) < 'A' || toupper(grid[5]) > 'X') return false;
+        if (toupper(grid[4]) < 'A' || toupper(grid[4]) > 'X') return false;
+    }
+
+    if (len >= 4)
+    {
+        if (grid[3] < '0' || grid[3] > '9') return false;
+        if (grid[2] < '0' || grid[2] > '9') return false;
+    }
+
+    if (toupper(grid[1]) < 'A' || toupper(grid[1]) > 'R') return false;
+    if (toupper(grid[0]) < 'A' || toupper(grid[0]) > 'R') return false;
     
     return true;
 }
@@ -68,64 +80,139 @@ const char *pos_grid(double lat, double lon) {
         lon = 179.99999;
     }
 
+    /* Jerry Shaw K6ANI
+    Added overrange checks to t1. Changed sructure
+    to be consistent with new checks.Changed syntax to
+    be consistent with char/int/double types.
+    */
+
     lon += 180.0;
-    t1 = (int)(lon / 20);
+
+    t1 = (int)(lon / 20.0);
+
+    if (t1 < 0) {
+        t1 = 0;
+    } else if (t1 > ('R' - 'A')) {
+            t1 = ('R' - 'A');
+    }
+
     buf[0] = (char)t1 + 'A';
 
-    if ('R' < buf[0]) {
-        buf[0] = 'R';
+    lon -= (double)t1 * 20.0;
+
+    t1 = (int)(lon / 2.0);
+
+    if (t1 < 0) {
+        t1 = 0;
     }
-    lon -= (float)t1 * 20.0;
-
-    t1 = (int)lon / 2;
-    buf[2] = (char)t1 + '0';
-    lon -= (float)t1 * 2;
-
-    lon *= 60.0;
-
-    t1 = (int)(lon / 5);
-    buf[4] = (char) ((char)t1 + 'a');
-    lon -= (float)(t1 * 5);
-
-    lon *= 60.0;
-    t1 = (int)(lon / 30);
-
-    if (9 < t1) {
+    else if (t1 > 9) {
         t1 = 9;
     }
-    buf[6] = (char) ((char)t1 + '0');
+    
+    buf[2] = (char)t1 + '0';
+
+    lon -= (double)t1 * 2.0;
+
+    lon *= 60.0;
+
+    /* Jerry Shaw K6ANI
+    buf[4] is now in the range 'A' to 'X'.
+    */
+
+    t1 = (int)(lon / 5.0);
+
+    if (t1 < 0) {
+        t1 = 0;
+    }
+    else if (t1 > ('X' - 'A')) {
+        t1 = ('X' - 'A');
+    }
+
+    buf[4] = (char)t1 + 'A';
+
+    lon -= (double)t1 * 5.0;
+
+    lon *= 60.0;
+
+    t1 = (int)(lon / 30.0);
+
+    if (t1 < 0) {
+        t1 = 0;
+    }
+    else if (t1 > 9) {
+        t1 = 9;
+    }
+
+    buf[6] = (char)t1 + '0';
 
     if (89.99999 < lat) {
         lat = 89.99999;
     }
 
     lat += 90.0;
+
     t1 = (int)(lat / 10.0);
+
+    if (t1 < 0) {
+        t1 = 0;
+    }
+    else if (t1 > ('R' - 'A')) {
+        t1 = ('R' - 'A');
+    }
+
     buf[1] = (char)t1 + 'A';
 
-    if ('R' < buf[1]) {
-        buf[1] = 'R';
+    lat -= (double)t1 * 10.0;
+
+    t1 = (int)lat;
+
+    if (t1 < 0) {
+        t1 = 0;
     }
-    lat -= (float)t1 * 10.0;
-
-    buf[3] = (char)lat + '0';
-    lat -= (int)lat;
-    lat *= 60.0;
-
-    t1 = (int)(lat / 2.5);
-    buf[5] = (char)((char)t1 + 'a');
-    lat -= (float)(t1 * 2.5);
-    lat *= 60.0;
-
-    t1 = (int)(lat / 15);
-
-    if (9 < t1) {
+    else if (t1 > 9) {
         t1 = 9;
     }
 
-    buf[7] = (char) ((char)t1 + '0');
-    buf[8] = '\0';
+    buf[3] =(char)t1 + '0';
 
+    lat -= (double)t1;
+
+    lat *= 60.0;
+
+    /* Jerry Shaw K6ANI
+    buf[5] is now in the range 'A' to 'X'.
+    */
+
+    t1 = (int)(lat / 2.5);
+
+    if (t1 < 0) {
+        t1 = 0;
+    }
+    else if (t1 > ('X' - 'A')) {
+        t1 = ('X' - 'A');
+    }
+
+    buf[5] = (char)t1 + 'A';
+
+    lat -= (double)t1 * 2.5;
+
+    lat *= 60.0;
+
+    t1 = (int)(lat / 15.0);
+
+
+    if (t1 < 0) {
+        t1 = 0;
+    }
+    else if (t1 > 9) {
+        t1 = 9;
+    }
+
+
+    buf[7] = (char)t1 + '0';
+
+    buf[8] = '\0';
+    
     return buf;
 }
 
@@ -135,6 +222,17 @@ void grid_pos(const char *grid, double *lat, double *lon) {
     *lon = -180.0;
     *lat = -90.0;
     
+    /* Jerry Shaw K6ANI
+    Added check for valid grid. Return lat/lon 0
+    if not valid.
+    */
+
+    if (!grid_check(grid)) {
+        *lat = 0.0;
+        *lon = 0.0
+            return;
+    }
+
     *lon += (toupper(grid[0]) - 'A') * 20.0;
     *lat += (toupper(grid[1]) - 'A') * 10.0;
     
@@ -178,6 +276,15 @@ void grid_pos(const char *grid, double *lat, double *lon) {
 int32_t grid_dist(const char *grid) {
     double lat = 0;
     double lon = 0;
+
+    /* Jerry Shaw K6ANI
+    Added check for valid grid. Return distance 0
+    if not valid.
+    */
+
+    if (!grid_check(grid)) {
+        return 0.0;
+    }
 
     grid_pos(grid, &lat, &lon);
     
